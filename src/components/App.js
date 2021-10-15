f18b2cbc350c43d34f222655c4b656a920d77436
hideo <neildaikoku@gmail.com>
2021-10-15 16:25:51 +0900

    add electron start

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        ME Prototype
+      </header>
+    </div>
+  );
+}
+
+export default App;
